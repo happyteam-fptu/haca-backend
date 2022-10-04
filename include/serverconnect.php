f54f80f9eb734cfe5194149a1e5c7060931d8b0b
Tunna Duong <tunnaduong@gmail.com>
2022-10-05 00:43:07 +0700

    moved mysqli_set_charset to inside try catch, added jwt function

--- include/serverconnect.php
+++ include/serverconnect.php
@@ -32,10 +32,10 @@ try {
     $db = mysqli_connect($maychu, $tendangnhap, $matkhau, $tendb);
     $conn = new mysqli($maychu, $tendangnhap, $matkhau, $tendb);
     $con = mysqli_connect($maychu, $tendangnhap, $matkhau, $tendb);
+    // Mã này giúp cho trang khỏi bị các ký tự Unicode kì lạ
+    mysqli_set_charset($conn, 'UTF8');
+    mysqli_set_charset($db, 'UTF8');
+    mysqli_set_charset($con, 'UTF8');
 } catch (Exception $err) {
     die(json_encode(array("error" => "misconfigured_db_connect_failed", "detail" => "Có lỗi khi kết nối với database! Thử kiểm tra các thông tin đăng nhập và đảm bảo đã port 3306 đã được cho phép bởi tường lửa của server...")));
 }
-// Mã này giúp cho trang khỏi bị các ký tự Unicode kì lạ
-mysqli_set_charset($conn, 'UTF8');
-mysqli_set_charset($db, 'UTF8');
-mysqli_set_charset($con, 'UTF8');
--- routes.php
+++ routes.php
@@ -7,22 +7,58 @@
 
 // Require router file for URL routing logic to works 
 require_once("{$_SERVER['DOCUMENT_ROOT']}/include/router.php");
-// Require database server connection credential establishment file for database queries to works
-require_once("{$_SERVER['DOCUMENT_ROOT']}/include/serverconnect.php");
 
 // Return access denied error page when user open the API link
 get('/', 'errors/403.php');
 
 // Handling login backend logic
 any('/v1.0/auth/login', function () {
+    // Require database server connection credential establishment file for database queries to works
+    require_once("{$_SERVER['DOCUMENT_ROOT']}/include/serverconnect.php");
+    // Set response type header to JSON for better compatibility
+    header('Content-Type: application/json; charset=utf-8');
     // If method is GET then API is not supported, return message
     if ($_SERVER['REQUEST_METHOD'] == "GET")
         echo json_encode(array("error" => "method_not_supported", "detail" => "API không hỗ trợ method GET!"));
     else if ($_SERVER['REQUEST_METHOD'] == "POST") {
         // If method is POST then continue the logic
         if (isset($_POST['username']) && isset($_POST['password'])) {
-            // TODO: Handling authentication logic with database
-
+            // Handling authentication logic with database using prepared statements
+            $stmt = $conn->prepare("SELECT username FROM se1741_students WHERE username=? LIMIT 1");
+            $stmt->bind_param("s", $_POST['username']);
+            $stmt->execute();
+            $stmt->store_result();
+            if ($stmt->num_rows == 1) {
+                $stmt = $conn->prepare("SELECT name, last_name, username, member_code FROM se1741_students WHERE username=? AND password=? LIMIT 1");
+                $stmt->bind_param("ss", $_POST['username'], $_POST['password']);
+                $stmt->execute();
+                $stmt->bind_result($name, $last_name, $username, $member_code);
+                $stmt->store_result();
+                // To check if the row exists
+                if ($stmt->num_rows == 1) {
+                    // Fetching the contents of the row
+                    if ($stmt->fetch()) {
+                        echo json_encode(array(
+                            "status" => "success",
+                            "status_code" => "user_auth_success",
+                            "message" => "Đăng nhập thành công!",
+                            "auth_data" => array("access_token" => "123", "refresh_token" => "abc")
+                        ));
+                    }
+                } else {
+                    echo json_encode(array(
+                        "status" => "failed",
+                        "status_code" => "user_auth_failed_wrong_pass",
+                        "message" => "Mật khẩu sai! Vui lòng thử lại..."
+                    ));
+                }
+            } else {
+                echo json_encode(array(
+                    "status" => "failed",
+                    "status_code" => "user_auth_failed_wrong_username",
+                    "message" => "Tên đăng nhập sai! Vui lòng thử lại..."
+                ));
+            }
         } else
             // If missing or wrong parameters then return an error
             echo json_encode(array("error" => "wrong_or_missing_params", "detail" => "Tham số bạn truyền vào cho API đang thiếu hoặc bị sai!"));
