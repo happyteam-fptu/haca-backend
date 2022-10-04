cd445d79a8c30bb4c44681b97c7d8e74733580c0
Tunna Duong <tunnaduong@gmail.com>
2022-10-05 03:28:55 +0700

    added some friendly comments

--- routes.php
+++ routes.php
@@ -51,6 +51,9 @@ any("/v1.0/auth/login", function () {
             $stmt->bind_param("s", $_POST['username']);
             $stmt->execute();
             $stmt->store_result();
+            // We should use prepared statement, not normal SQL syntax. Because as I can see from my eyes,
+            // many websites has this XSS or SQL Injection Bug. It might be harder to review the code, but
+            // just in case there is some hacker curiously hacked our server :D
             if ($stmt->num_rows == 1) {
                 $stmt = $conn->prepare("SELECT username, student_id, role FROM se1741_students WHERE username=? AND password=? LIMIT 1");
                 $stmt->bind_param("ss", $_POST['username'], $_POST['password']);
@@ -72,6 +75,7 @@ any("/v1.0/auth/login", function () {
                         // Usually expires in 1 month
                         $JWT_REFRESH_EXPIRE = getenv("JWT_REFRESH_TOKEN_EXPIRE_TIME");
                         // Handling generating access token logic here
+                        // Setting JWT payload variables
                         $payload = array();
                         $payload['token_type'] = "access";
                         $payload['iat'] = strtotime(date("Y-m-d H:i:s"));
@@ -79,12 +83,18 @@ any("/v1.0/auth/login", function () {
                         $payload['uname'] = $username;
                         $payload['stid'] = $student_id;
                         $payload['role'] = $role;
+                        // Encode these data with JWT key in .env into JSON Web Token
                         $token = JWT::encode($payload, $JWT_KEY);
+                        // Clone current payload for access token into refresh token, save in other variable
                         $rpayload = $payload;
+                        // Setting token type to refresh
                         $rpayload['token_type'] = "refresh";
                         $rpayload['iat'] = strtotime(date("Y-m-d H:i:s"));
+                        // Change expire time to longer duration
                         $rpayload['exp'] = strtotime(date("Y-m-d H:i:s")) + $JWT_REFRESH_EXPIRE;
+                        // Encode these data with JWT key in .env into JSON Web Token
                         $rtoken = JWT::encode($rpayload, $JWT_KEY);
+                        // Return success status if both username and password are correct
                         echo json_encode(array(
                             "status" => "success",
                             "status_code" => "user_auth_success",
@@ -93,6 +103,7 @@ any("/v1.0/auth/login", function () {
                         ));
                     }
                 } else {
+                    // Return an error if the password is wrong
                     echo json_encode(array(
                         "status" => "failed",
                         "status_code" => "user_auth_failed_wrong_pass",
@@ -100,6 +111,7 @@ any("/v1.0/auth/login", function () {
                     ));
                 }
             } else {
+                // Return an error if there is no user exist with that inputted name
                 echo json_encode(array(
                     "status" => "failed",
                     "status_code" => "user_auth_failed_unknown_user",
@@ -121,7 +133,10 @@ any("/v1.0/auth/login", function () {
 get('/v1.0/student/info', function () {
     // Set response type header to JSON for better browser compatibility
     header('Content-Type: application/json; charset=utf-8');
-    auth_verify(getBearerToken());
+    if (auth_verify(getBearerToken())) {
+        // If having and accepted bearer token within the request header then continue the below code...
+        echo "Tunga nh dziaiii";
+    }
 });
 
 // ##################################################
