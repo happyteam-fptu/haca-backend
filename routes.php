14abfb4752dbe06528f0200f33fdb003b13361fa
Tunna Duong <tunnaduong@gmail.com>
2022-10-05 16:22:08 +0700

    added cors

--- routes.php
+++ routes.php
@@ -10,6 +10,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// CORS Header allow request from any IP
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: *");
+
 // Require router file for URL routing logic to works 
 require_once("{$_SERVER['DOCUMENT_ROOT']}/include/router.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/include/authverify.php");
